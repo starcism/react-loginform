bb5d4b44baf166132848cc75bffae41dba7719e0
sangmoon <laugh120@naver.com>
2023-01-18 18:49:48 +0900

    loginform

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,26 +1,45 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { useCallback, useState } from 'react';
+import { InputWrapper, LginFrmBody, LginFrmContainer, LginFrmWrapper, SelectorWrapper, StyledLink } from './loginFormStyle';
+import SignIn from './Signin';
+import SignUp from './Signup';
 
 const LoginForm = () => {
+    const [isSignIn, setIsSignIn] = useState(true);
+    const handleSignIn = useCallback(()=>{
+        if(isSignIn){
+            setIsSignIn(false)
+        } else {
+            setIsSignIn(true)
+        }
+    }, [isSignIn]);
+
     return (
-        <Container>
-            <StyledLink to="/sign_in">로그인</StyledLink>
-            <StyledLink to="/sign_up">회원가입</StyledLink>  
-        </Container>
+        <LginFrmContainer>
+            <LginFrmWrapper>
+                { isSignIn ? (
+                    <LginFrmBody>
+                        <SelectorWrapper>
+                            <StyledLink className={ isSignIn && "router-active" }>로그인  </StyledLink>
+                            <StyledLink to="/sign_up" onClick={handleSignIn}>회원가입</StyledLink>
+                        </SelectorWrapper>
+                        <InputWrapper>
+                            <SignIn />
+                        </InputWrapper>
+                    </LginFrmBody>
+                    ) : (
+                    <LginFrmBody>
+                        <SelectorWrapper>
+                            <StyledLink to="/sign_in" onClick={handleSignIn}>로그인  </StyledLink>
+                            <StyledLink className={ !isSignIn && "router-active" }>회원가입</StyledLink>
+                        </SelectorWrapper>
+                        <InputWrapper>
+                            <SignUp />
+                        </InputWrapper>
+                    </LginFrmBody>
+                    )}
+            </LginFrmWrapper>
+        </LginFrmContainer>
     );
 }
-
-const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    padding: 0;
-    margin-top: 10px;
-    margin-bottom: 0;
-`
-const StyledLink = styled(Link)`
-    margin-left: 5px;
-    margin-right: 5px;
-    font-size: 30px;
-`
 
 export default LoginForm;
